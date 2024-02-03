6e3a3ccd160bc1c98e789e56790f29ffb3b5dcb3
Ivan Mirić <1009277+imiric@users.noreply.github.com>
2024-02-03 20:52:54 +0100

    wip

--- zsh-fzf-pass.zsh
+++ zsh-fzf-pass.zsh
@@ -1,6 +1,7 @@
 function fuzzy-pass() {
   PASS_STR_VALID_DIR="${PASSWORD_STORE_DIR:-${HOME}/.password-store}"
-  PASSFILE=$(find -L $PASS_STR_VALID_DIR | sed "s|$PASS_STR_VALID_DIR||" | grep '.gpg' | sed 's/.gpg$//g' | fzf)
+  FZF_ARGS=("--select-1" "--exit-0" "--height=25%" "--reverse" "--tac" "--no-sort")
+  PASSFILE=$(find -L $PASS_STR_VALID_DIR | sed "s|$PASS_STR_VALID_DIR||" | grep '.gpg' | sed 's/.gpg$//g' | fzf "${FZF_ARGS[@]}")
 
   [ -z "$PASSFILE" ] && return 0
 
@@ -20,7 +21,7 @@ function fuzzy-pass() {
   ACTIONS="Edit\nFile"
 
   if [ -n "${URL}" ]; then
-    ACTIONS="Url\n${ACTIONS}"
+    ACTIONS="URL\n${ACTIONS}"
   fi
   if [ -n "${EMAIL}" ]; then
     ACTIONS="Email\n${ACTIONS}"
@@ -36,7 +37,7 @@ function fuzzy-pass() {
 
   while ${CONTINUE}; do
     ACTION=$(echo "${ACTIONS}" \
-      | fzf --height 10 --border --header "Pass file ${PASSFILE}")
+      | fzf --header "Pass file ${PASSFILE}" "${FZF_ARGS[@]}")
     case ${ACTION} in
       Login)
         echo "${LOGIN}" | clipcopy
@@ -46,7 +47,7 @@ function fuzzy-pass() {
         pass --clip "${PASSFILE}" 1>/dev/null
         echo "Copied Password to clipboard (clear in 45 seconds)"
         ;;
-      Url)
+      URL)
         echo "${URL}" | clipcopy
         echo "Copied Url '${URL}' to clipboard"
         ;;
